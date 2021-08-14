1d970e807c98058f8f5588910c17562e93dd66ed
bramvdzalm <69297451+bramvdzalm@users.noreply.github.com>
2021-08-14 14:40:27 +0200

    first commit

---
+++ farm.js
@@ -0,0 +1,21 @@
+const getYieldForPlant = (corn) => {
+    return corn.yield
+  }
+
+const getYieldForCrop = (crop, corn) => {
+    return crop.numCrops * getYieldForPlant(crop.crop, corn)
+  }
+  
+const getTotalYield = ({ crops }, influencingFactors) => {
+    let total = 0;
+    crops.forEach((crop) => {
+        total += getYieldForCrop(crop, influencingFactors);
+    });
+    return total;
+  };
+
+
+
+
+
+module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield }
